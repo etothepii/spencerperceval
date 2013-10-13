ca25c86bad033dade035c7943ec7106a2d4390a8
jrrpl <jr@epii.co.uk>
2013-10-13 03:20:16 +0100

    Added Tuple Equals and Hash

--- src/main/java/uk/co/epii/spencerperceval/tuple/Quadruple.java
+++ src/main/java/uk/co/epii/spencerperceval/tuple/Quadruple.java
@@ -57,4 +57,28 @@ public class Quadruple<F, S, T, G> {
     public void setFourth(G fourth) {
         this.fourth = fourth;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Quadruple quadruple = (Quadruple) o;
+
+        if (first != null ? !first.equals(quadruple.first) : quadruple.first != null) return false;
+        if (fourth != null ? !fourth.equals(quadruple.fourth) : quadruple.fourth != null) return false;
+        if (second != null ? !second.equals(quadruple.second) : quadruple.second != null) return false;
+        if (third != null ? !third.equals(quadruple.third) : quadruple.third != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        result = 31 * result + (third != null ? third.hashCode() : 0);
+        result = 31 * result + (fourth != null ? fourth.hashCode() : 0);
+        return result;
+    }
 }
