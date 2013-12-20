93e7268ec8a46c604de9533820efe225ff7e7ba7
jrrpl <jr@epii.co.uk>
2013-12-20 20:48:46 +0000

    A type safe place holder

--- src/main/java/uk/co/epii/spencerperceval/tuple/Single.java
+++ src/main/java/uk/co/epii/spencerperceval/tuple/Single.java
@@ -1,10 +1,52 @@
 package uk.co.epii.spencerperceval.tuple;
 
 /**
- * Created with IntelliJ IDEA.
- * User: jrrpl
- * Date: 20/12/2013
- * Time: 20:46
+ * User: James Robinson
+ * Date: 30/09/2013
+ * Time: 23:04
  */
-public class Single {
+public class Single <F> {
+
+    private F first;
+
+    public Single() {
+        first = null;
+    }
+
+    public Single(F first) {
+        this.first = first;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Single single = (Single) o;
+
+        if (first != null ? !first.equals(single.first) : single.first != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Duple{" +
+                "first=" + first +
+                '}';
+    }
 }
